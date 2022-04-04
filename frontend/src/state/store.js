07bac2cb08736a715d745a9473ebe98d8c3992a9
이홍준 <zunza96@naver.com>
2022-04-04 14:47:24 +0900

    FEAT: create userSlice

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tabIndexReducer from "./tabIndexSlice";
 import priceRangeReducer from './priceRangeSlice';
+import userReducer from './userSlice';
 export const store = configureStore({
   reducer: {
     tabIndex : tabIndexReducer,
     priceRange: priceRangeReducer,
+    user: userReducer,
   },
 });
---
+++ frontend/src/state/userSlice.js
@@ -0,0 +1,30 @@
+// const [profileBackgroundImage,setProfileBackgroundImage] = useState("https://as1.ftcdn.net/v2/jpg/02/70/64/54/1000_F_270645457_FR4CBhmmKSNqn4hk0X21PPzu4FuXLGxR.jpg");
+// const [profileImage,setProfileImage] = useState("assets/default_profile.jpg");
+// const [nickname,setNickname] = useState("nickname");
+// const [walletAddress,setWalletAddress] = useState("0xc2d8FcB473A1b400FED7B2b1d4c1453CB14ABdwq");
+// const [bio,setBio] = useState("Hello!");
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        value:{
+            userId: 0,
+            email: "",
+            walletAddress: "",
+            nickname: "",
+            profileImageUrl: "",
+            bio: ""
+        }
+    },
+    reducers: {
+        changeTabIndex: (state, action)=>{
+            state.value = action.payload;
+        },
+    }
+});
+
+export const {changeTabIndex} = userSlice.actions;
+
+export default userSlice.reducer;
