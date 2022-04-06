1b81edd6eb4bf4d9e53bc38d711686a4a800980b
kisung <abc1242@naver.com>
2022-04-06 17:19:46 +0900

    fix : 랭킹조회시 오타 수정

--- backend/src/donation/donation.repository.js
+++ backend/src/donation/donation.repository.js
@@ -15,7 +15,7 @@ class DonationRepository{
     //기부랭킹 10등까지 가져오기
     async getRank(){
         await connection.query(`set @a := 0;`);
-        var a = await connection.query( `select ( @a := @a+1 ) as s, user.profile_image_url, user.nickname, sum(price) as amount from donation,user where donation.user_id = user.user_id group by user.user_id order by sum(price) desc limit 10;`);
+        var a = await connection.query( `select ( @a := @a+1 ) as rankId, user.profile_image_url, user.nickname, sum(price) as amount from donation,user where donation.user_id = user.user_id group by user.user_id order by sum(price) desc limit 10;`);
         
         return a[0];
     }
